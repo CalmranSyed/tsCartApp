3c6ffa51b36a3b0c1316aee6dfe601bfd442a1ed
CalmranSyed <68997464+CalmranSyed@users.noreply.github.com>
2022-07-03 02:44:57 +0530

    Added entity class for conversion and tried to rewrite repeated products logic

--- main.ts
+++ main.ts
@@ -11,13 +11,13 @@ interface ProductStructure {
     category: number;
 }
 
-interface CartProduct {
+interface CartProductStructure {
     product: ProductStructure;
     counter: number;
 }
 
 interface CartActions {
-    addItem(product: ProductStructure): void;
+    addItem(productID: string): void;
     // removeItem(): string;
     // incrementItem(): string;
     // decrementItem(): string;
@@ -35,7 +35,6 @@ class NetworkCall {
 }
 
 // Product Service
-
 class ProductService {
 
     // render product by taking data from the products array
@@ -74,68 +73,76 @@ class ProductService {
 
 }
 
-// Cart Class (Handles logic and UI)
-class Cart implements CartActions {
+// 
+class CartModel {
+
+    addListeners(buttons: HTMLCollection) {
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener("click", (e: Event) => {
+                e.preventDefault();
+                cHandler.addItem(products[i].id);
+                cHandler.checkEmpty();
+            });
+        }
+    }
+}
+
+// CartHandler Class (Handles UI)
+class CartHandler implements CartActions {
     cartItems: CartProduct[] = [];
 
     get items(): CartProduct[] {
         return this.cartItems;
     }
 
-    addListeners(buttons: HTMLCollection) {
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].addEventListener("click", (e: Event) => {
-                e.preventDefault();
-                this.addItem(products[i]);
-                this.checkEmpty();
-            });
-        }
 
-    }
-
-    addItem(product: ProductStructure): void {
-        this.cartItems.push({ product: product, counter: 1 });
+    addItem(productID: string): void {
 
 
         // check for repeated products
-        for (let i = 0; i < this.cartItems.length; i++) {
+        // for (let i = 0; i < this.cartItems.length; i++) {
 
-            // getter for added product
-            var addedProduct = document.getElementsByClassName('cart-item');
+        // getter for added product
+        var addedProduct = document.getElementsByClassName('cart-item');
 
-            var cartItemTemplate = `
-            <div class="product cart-item card mb-3">
-                <div class="row g-0 align-items-center">
-                    <div class="col-md-4">
-                        <img src="${this.cartItems[i].product.img_url}" class="img-fluid rounded-start" alt="${this.cartItems[i].product.title}">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body p-3">
-                            <h5 class="card-title">${this.cartItems[i].product.title}</h5>
-                            <p class="card-text product-price">
-                                <span>Price :</span>
-                                <span class="original-price text-decoration-line-through text-muted me-2">${this.cartItems[i].product.orig_price} Rs</span>
-                                <span class="discount-price fw-bold">${this.cartItems[i].product.disc_price} Rs</span>
-                            </p>
-                            <p class="item-quantity-wrap">
-                                <span>Quantity :</span>
-                                <span id="item-quantity">${this.cartItems[i].counter}</span>
-                            </p>
-                            <div>
-                                <button class="incrementItem me-2 btn btn-outline-dark"><i class="bi bi-plus-lg"></i></button>
-                                <button class="decrementItem me-2 btn btn-outline-dark"><i class="bi bi-dash-lg"></i></button>
-                                <button class="removeItem btn btn-outline-dark">Remove</button>
-                            </div>
-                        </div>  
-                    </div>
+        /* var cartItemTemplate = `
+        <div class="product cart-item card mb-3">
+            <div class="row g-0 align-items-center">
+                <div class="col-md-4">
+                    <img src="${this.cartItems[i].product.img_url}" class="img-fluid rounded-start" alt="${this.cartItems[i].product.title}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body p-3">
+                        <h5 class="card-title">${this.cartItems[i].product.title}</h5>
+                        <p class="card-text product-price">
+                            <span>Price :</span>
+                            <span class="original-price text-decoration-line-through text-muted me-2">${this.cartItems[i].product.orig_price} Rs</span>
+                            <span class="discount-price fw-bold">${this.cartItems[i].product.disc_price} Rs</span>
+                        </p>
+                        <p class="item-quantity-wrap">
+                            <span>Quantity :</span>
+                            <span id="item-quantity">${this.cartItems[i].counter}</span>
+                        </p>
+                        <div>
+                            <button class="incrementItem me-2 btn btn-outline-dark"><i class="bi bi-plus-lg"></i></button>
+                            <button class="decrementItem me-2 btn btn-outline-dark"><i class="bi bi-dash-lg"></i></button>
+                            <button class="removeItem btn btn-outline-dark">Remove</button>
+                        </div>
+                    </div>  
                 </div>
             </div>
-                `;
+        </div>
+            `; */
 
-            var cart = document.getElementById('cartBody');
-            cart.insertAdjacentHTML('beforeend', cartItemTemplate);
 
-        }
+        var cart = document.getElementById('cartBody');
+
+
+        // cart.insertAdjacentHTML('beforeend', cartItemTemplate);
+
+        // }
+
+
 
         var itemQuantity = document.getElementById('item-quantity');
 
@@ -171,14 +178,23 @@ class Cart implements CartActions {
 
 }
 
+var cHandler = new CartHandler();
 
+// Cart entity class (For conversion of added objects)
+class CartProduct {
+    id: string;
+    title: string;
+    price: number;
+    count: number;
 
-// Cart entity class
-// class CartProduct {
-//     id: string;
-//     title: string;
-//     count: number;
-// }
+    constructor(id: string, title: string, price: number, count: number) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.count = count;
+    }
+}
+
 
 var products: ProductStructure[] = [];
 
@@ -195,7 +211,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 
         const prodService = new ProductService();
-        const c = new Cart();
+        const c = new CartHandler();
 
         prodService.showProducts(products);
         prodService.showCount(products);
