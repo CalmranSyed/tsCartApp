3853ac42af4458617481123fc4d909160e3b5edd
Salman Khan <admin.gameup@protonmail.com>
2022-07-07 03:33:32 +0530

    Replace template literal with concrete implementation

--- main.ts
+++ main.ts
@@ -57,15 +57,15 @@ document.addEventListener("DOMContentLoaded", () => {
         prodService.showProducts(products);
         prodService.showCount(products);
 
-        var prodInterval = setInterval(() => {
-            var buttons = document.getElementsByClassName("add-to-cart") as HTMLCollection;
+        // var prodInterval = setInterval(() => {
+        //     var buttons = document.getElementsByClassName("add-to-cart") as HTMLCollection;
 
-            if (buttons.length == products.length) {
-                cModel.addListeners(buttons);
-                clearInterval(prodInterval);
-            }
+        //     if (buttons.length == products.length) {
+        //         cModel.addListeners(buttons);
+        //         clearInterval(prodInterval);
+        //     }
 
-        }, 200);
+        // }, 200);
 
 
     });
@@ -81,26 +81,7 @@ class ProductService {
         var prodWrap = document.getElementById("products-wrapper");
 
         products.forEach((product) => {
-            // HTML layout for product card
-            var productTemplate = `
-      <div class="col-md-3 g-4 product">
-        <div class="card h-100 ">
-          <img src="${product.img_url}" class="card-img-top product-img p-3 border-bottom" alt="${product.title}">
-        <div class="card-body">
-          <h5 class="card-title product-title">${product.title}</h5>
-          <p class="card-text product-price">
-            <span>Price :</span>
-            <span class="original-price text-decoration-line-through text-muted me-2">${product.orig_price} Rs</span>
-            <span class="discount-price fw-bold">${product.disc_price} Rs</span>
-          </p>
-          <p class="card-text product-description">${product.desc}</p>
-          <a href="#" class="btn btn-dark add-to-cart">Add To Cart</a>
-        </div>
-        </div>
-      </div>
-      `;
-
-            prodWrap.insertAdjacentHTML("beforeend", productTemplate);
+            prodWrap.appendChild(createProducts(product))
         });
     }
 
@@ -112,22 +93,70 @@ class ProductService {
 
 }
 
+function createProducts(product: ProductStructure): HTMLDivElement {
+    var root = document.createElement("div")
+    root.setAttribute("class", "col-md-3 g-4 product")
+
+    var outerCard = document.createElement("div")
+    outerCard.setAttribute("class", "card h-100")
+
+    var thumbnail = document.createElement("img")
+    thumbnail.setAttribute("src", product.img_url)
+    thumbnail.setAttribute("class", "card-img-top product-img p-3 border-bottom")
+
+    var cardBody = document.createElement("div")
+    cardBody.setAttribute("class", "card-body")
+
+    var cardBodyTitle = document.createElement("h5")
+    cardBodyTitle.setAttribute("class", "card-title product-title")
+    cardBodyTitle.innerText = product.title
+
+    var cardDesc = document.createElement("p")
+    cardDesc.setAttribute("class", "card-text product-price")
+
+    var price = document.createElement("span")
+    price.innerText = "Price: "
+
+    var origPrice = document.createElement("span")
+    origPrice.setAttribute("class", "original-price text-decoration-line-through text-muted me-2")
+    origPrice.innerText = product.orig_price.toString()
+
+    var discPrice = document.createElement("span")
+    discPrice.setAttribute("class", "discount-price fw-bold")
+    discPrice.innerText = product.disc_price + " Rs."
+
+    var prodDesc = document.createElement("p")
+    prodDesc.setAttribute("class", "card-text product-description")
+    prodDesc.innerText = product.desc
+
+    var anchor = document.createElement("a")
+    anchor.setAttribute("class", "btn btn-dark add-to-cart")
+    anchor.innerText = "Add to Cart"
+    anchor.addEventListener("click", () => {
+        console.log(product.id)
+        cHandler.onclickAddToCart(product);
+        cHandler.checkEmpty();
+    })
+
+    cardDesc.appendChild(price)
+    cardDesc.appendChild(origPrice)
+    cardDesc.appendChild(discPrice)
+
+    cardBody.appendChild(cardBodyTitle)
+    cardBody.appendChild(cardDesc)
+    cardBody.appendChild(prodDesc)
+    cardBody.appendChild(anchor)
+
+    outerCard.appendChild(thumbnail)
+    outerCard.appendChild(cardBody)
+    root.appendChild(outerCard)
+
+    return root
+}
+
 // 
 class CartModel {
     cart: CartProduct[] = []
-
-
-    addListeners(buttons: HTMLCollection) {
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].addEventListener("click", (e: Event) => {
-                e.preventDefault();
-                // cHandler.addToCart()
-
-                cHandler.onclickAddToCart(products[i]);
-                cHandler.checkEmpty();
-            });
-        }
-    }
 }
 
 class ICartListener implements CartListener {
@@ -138,13 +167,13 @@ class ICartListener implements CartListener {
 }
 
 interface CartListener {
-    OnCartUpdate(cartProducts: CartProduct[]):any
+    OnCartUpdate(cartProducts: CartProduct[]): any
 }
 
 const cModel = new CartModel();
 // CartHandler Class (Handles UI)
 class CartHandler implements CartActions {
-    
+
     private _listener: CartListener;
     public get listener(): CartListener {
         return this._listener;
@@ -161,9 +190,9 @@ class CartHandler implements CartActions {
 
     onclickAddToCart(product: ProductStructure) {
         var cartProduct = new CartProduct(product.id, product.title, product.disc_price, 1)
-        
+
         //  When cart is empty
-        if(cModel.cart.length == 0) {
+        if (cModel.cart.length == 0) {
             cModel.cart.push(cartProduct)
             this.listener.OnCartUpdate(cModel.cart)
             return
@@ -179,7 +208,7 @@ class CartHandler implements CartActions {
 
         cModel.cart.push(cartProduct)
         this.listener.OnCartUpdate(cModel.cart)
-        
+
         console.log("Cart count : ", cModel.cart)
     }
 
